130f41e037471dcc46c55d5a30b9ee978f0b08f5
papaurke <urosmiric88@gmail.com>
2024-07-06 18:16:02 +0200

    feat(pages) Add orders and products page

---
+++ app/admin/orders/page.tsx
@@ -0,0 +1,7 @@
+export default function Orders() {
+  return (
+    <section className="flex items-center justify-center p-3 md:p-12 w-full">
+      <h2>Orders</h2>
+    </section>
+  );
+}
---
+++ app/admin/products/page.tsx
@@ -0,0 +1,7 @@
+export default function Products() {
+  return (
+    <section className="flex items-center justify-center p-3 md:p-12 w-full">
+      <h2>Products</h2>
+    </section>
+  );
+}
